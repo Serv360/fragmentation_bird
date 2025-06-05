0d4e1bdb567f1d4e4c606f0185fa8326ac9f828a
Serv360 <serv360.lt@gmail.com>
2025-06-05 18:03:17 +0100

    Keeps only (site, year) with two surveys.
    Descriptive statistics shows that abundance is the
    sum of the two surveys. Therefore, (site, year)
    with only one have ~ half less observations.
    
    The other solution would be to double their
    abundance, but how to handle the different
    species ? It's not satisfactory.

--- development/bird_indicators.R
+++ development/bird_indicators.R
@@ -41,12 +41,13 @@ compute_category_indicators <- function(bird_data, traits_data) {
         Total_Abundance = rowSums(data),
         Species_Richness = rowSums(data > 0),
         Shannon_Diversity = diversity(data, index = "shannon"),
+        # H' = -\sum_1^S{p_i*log_2(p_i)} with p_i = n_i/N
+        # H' shannon index, S number of species, n_i abundance for species i
+        # N total abundance for all species
+        # Therefore, if there are species with abundance 0 it doesn't change it.
         Simpson_Diversity = diversity(data, index = "simpson")
       )
     })
-  
-  wide <- agg %>%
-    pivot_wider(names_from = pk_species, values_from = abundance, values_fill = 0)
   
   return(indicators_list)
 }
@@ -54,14 +55,33 @@ compute_category_indicators <- function(bird_data, traits_data) {
 load_data <- function(bird_path, species_path) {
   bird_data <- read.csv(bird_path, sep = ",")
   traits_data <- read.csv(species_path, sep = ",")
+  
+  # Uses "unknown" when a species is not in a category
+  traits_data <- traits_data %>%
+    mutate(habitat_specialisation = case_when(
+      habitat_specialisation %in% c("farmland", "urban", "woodland", "generalist") ~ habitat_specialisation,
+      TRUE ~ "unknown"
+    ))
+  
+  bird_data <- bird_data %>%
+    filter(passage == "OK_1_and_2") # Keeps only (site, year) with 2 passages
+  
   return(list(bird = bird_data, traits = traits_data))
 }
 
+write_results <- function(indicators, output_path) {
+  write.csv(indicators, output_path, row.names = FALSE)
+} 
+
 bird_path <- "C:/Users/Serv3/Desktop/Cambridge/Course/3 Easter/Dissertation EP/data/biodiversity/STOC/countingdata_2007_2023.csv"
 species_path <- "C:/Users/Serv3/Desktop/Cambridge/Course/3 Easter/Dissertation EP/data/biodiversity/STOC/species_stoc.csv"
+output_path <- "C:/Users/Serv3/Desktop/Cambridge/Course/3 Easter/Dissertation EP/data/biodiversity/div_indicators/indicators.csv"
 
 full_data <- load_data(bird_path, species_path)
 bird_data <- full_data$bird
 traits_data <- full_data$traits
 
 indicators <- compute_category_indicators(bird_data, traits_data)
+
+write_results(indicators, output_path)
+
